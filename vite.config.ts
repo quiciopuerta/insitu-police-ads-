a6d9d26bb4b0d074d9015a8104f62d9e3ee10aa3
Sanchez A. Franklin <sanchezfj@Sanchezs-MacBook-Pro.local>
2026-06-24 07:33:24 -0500

    fix(build): add VitePWA plugin to fix netlify build error resolving virtual:pwa-register/react

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 import { execSync } from 'child_process';
 import fs from 'fs';
 
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
@@ -41,7 +43,14 @@ export default defineConfig(({ mode }) => {
       }
     },
     plugins: [
-      react()
+      react(),
+      VitePWA({
+        registerType: 'prompt',
+        manifest: false,
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+        }
+      })
     ],
     build: {
       target: 'esnext',
